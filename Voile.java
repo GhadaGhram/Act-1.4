c3ed33e859b6f0cb29cf60d21afa30e68fd2e79a
GhadaGhram <103033611+GhadaGhram@users.noreply.github.com>
2022-04-08 22:54:59 +0200

    Update Voile.java

--- Voile.java
+++ Voile.java
@@ -1,15 +1,18 @@
-class Voile{
-     private int longueur ;
-     private int largeur;
-     protected int position ;
-     public String couleur;
-     private boolean enroule;
-     private boolean deroule;
+package projectBoat;
+public class Voile{
+       private id=6;
+       private int longueur ;
+       private int largeur;
+       protected String forme ;
+       public String couleur;
+       private boolean enroule;
+       private boolean deroule;
 
-     Voile()
-      {
+     public Voile(int longueur,int largeur)
+      {        this.longueur=0;
+               this.largeur=0;
       }
-     Voile(int longeur,int largeur,int position,String couleur,boolean enroule,boolean deroule)
+     public Voile(int longeur,int largeur,int position,String couleur,boolean enroule,boolean deroule)
      {
                this.longueur=longueur;
                this.largeur=largeur;
@@ -27,4 +30,62 @@ class Voile{
      boolean derouler (boolean deroule) { 
                                                                         boolean b=!deroule;
                                                                         return b;                                       
+    public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getLongueur() {
+		return longueur;
+	}
+
+	public void setLongueur(int longeur) {
+		this.longueur = longueur;
+	}
+
+	public int getLargeur() {
+		return largeur;
+	}
+
+	public void setLargeur(int largeur) {
+		this.largeur = largeur;
+	}
+
+	public String getForme() {
+		return forme;
+	}
+
+	public void setForme(String forme) {
+		this.forme = forme;
+	}
+
+	public String getCouleur() {
+		return couleur;
+	}
+
+	public void setCouleur(String couleur) {
+		this.couleur = couleur;
+	}
+
+	public boolean isEnroule() {
+		return enroule;
+	}
+
+	public void setEnroule(boolean enroule) {
+		this.enroule = enroule;
+	}
+
+	public boolean isDeroule() {
+		return deroule;
+	}
+
+	public void setDeroule(boolean deroule) {
+		this.deroule = deroule;
+	}
+	
+	
+}
      }}
