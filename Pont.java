a7340763661d7035fe70c819192f5861b432222a
GhadaGhram <103033611+GhadaGhram@users.noreply.github.com>
2022-04-08 23:22:58 +0200

    Update Pont.java

--- Pont.java
+++ Pont.java
@@ -1,19 +1,47 @@
-class Pont{
-          final int IDU4=4;
-          int x;
-          int y;
+public class Pont{
+          private int id=4;
+          public int x;
+          public int y;
           Voile voile;
            
-         Pont()
-         {
-             }
-          Pont(int x,  int y, Voile voile)
+         public Pont(int x,  int y)
+         {       this.x=0;
+                 this.y=0;}
+             
+          Pont(int x, int y, Voile voile)
         {
                  this.x=x;
                  this.y=y;
                  this.voile=voile;}
           
-          boolean test(Boolean test){ 
-
-                                        return(true);}
+          boolean test(Boolean test)
+          { 
+           return(true);}
+          
+          public int getId() {
+		return id;}
+	
+	public void setId(int id) {
+		this.id = id;}
+	
+          public int getX() {
+		return x;}
+	
+	public void setX(int x) {
+		this.x = x;}
+      
+          public int getY() {
+		return y;}
+	
+	public void setY(int y) {
+		this.y = y;}
+          
+          public Voile getVoile() {
+		return voile;}
+	
+	public void setVoile(Voile voile) {
+		this.voile = voile;}
+	
+          
+          
 }
